374730dff01e84908a1115475bea5b369efc8a67
Artyom Valitov <valitov.artem.pro@yandex.ru>
2022-11-14 18:52:05 +0500

    Setup: Navigation via BottomMenu realized

--- app/src/main/java/com/avalitov/showtime/main/MainActivity.kt
+++ app/src/main/java/com/avalitov/showtime/main/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.avalitov.showtime.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.avalitov.showtime.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.avalitov.showtime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Getting the NavHostFragment and NavController to use it further for app navigation
+        navHostFragment = binding.navHost.getFragment()
+        navController = navHostFragment.navController
+
+        // Tying the NavController with Bottom Navigation Menu
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
